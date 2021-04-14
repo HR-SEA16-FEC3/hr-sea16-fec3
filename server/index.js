36e3e69cdd321a2f752577998d8de9e1316c1e8b
zubair-akbar <62559732+zubair-akbar@users.noreply.github.com>
2021-04-13 21:52:26 -0400

    App.use(express.static) causing a GET 404 not found error point to Content Security policy

--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ const app = express();
 const PORT = 3000;
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, './public')));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 
 app.listen(PORT, () => {
   console.log(`Server is running and listening on port ${PORT}`);
