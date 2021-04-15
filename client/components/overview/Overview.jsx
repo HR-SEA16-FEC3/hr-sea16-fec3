1e96288d58f6e44b2d00059b10d11eab4dfc4c5e
Tony Ly <tonyjly@gmail.com>
2021-04-14 21:24:20 -0700

    render placeholder for Overview module

---
+++ client/components/overview/Overview.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Overview extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        Overview Template
+      </div>
+    );
+  }
+}
+
+export default Overview;
