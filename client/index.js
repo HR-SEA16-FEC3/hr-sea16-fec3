58d6e39f99edf5ed150cb6a88a9f3509c608833b
Jun Park <jun.park06@gmail.com>
2021-04-15 10:20:39 -0700

    idk what changed tbh

---
+++ client/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+const title = 'React with Webpack and Babel';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('App')
+);
