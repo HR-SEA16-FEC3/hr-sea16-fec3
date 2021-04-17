f4c205ded5464acd8e21d46793c05ed287b0585f
Tony Ly <tonyjly@gmail.com>
2021-04-17 16:10:32 -0700

    destructure props, add proptypes validation

--- client/components/overview/subcomponents/Style.jsx
+++ client/components/overview/subcomponents/Style.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Style = () => (
+const Style = (props) => {
+  const {
+    stylesList: {
+      product_id: id,
+    },
+  } = props;
 
-  <div data-testid="Style">
-    <div>Overview: Style Selector</div>
-    <br />
-  </div>
+  return (
 
-);
+    <div data-testid="Style">
+      <div>Overview: Style Selector</div>
+      {id}
+      <br />
+    </div>
+
+  );
+};
+
+Style.propTypes = {
+  stylesList: PropTypes.shape({
+    product_id: PropTypes.string.isRequired,
+  })
+};
 
 export default Style;
