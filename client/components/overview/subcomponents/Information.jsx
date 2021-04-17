1a5d9025469b032f7dfd076119e9abf73dc4af76
Tony Ly <tonyjly@gmail.com>
2021-04-17 16:10:25 -0700

    destructure props, add proptypes validation

--- client/components/overview/subcomponents/Information.jsx
+++ client/components/overview/subcomponents/Information.jsx
@@ -1,19 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Information = (props) => (
+// Star Rating (# of reviews)
+// Product Category
+// Product Title
+// Price
+// Product Overview
+// Share on Social Media
 
-  <div data-testid="Information">
-    <div>Overview: Product Information</div>
-    {/* Star Rating (# of reviews) */}
-    {/* Product Category */}
-    {/* Product Title */}
-    {/* Price */}
-    {/* Product Overview */}
-    {/* Share on Social Media */}
-    {props.infoList.id}
-    <br />
-  </div>
+function Information(props) {
+  const {
+    infoList: { // nested destructuring
+      id,
+      name,
+      slogan,
+      description,
+      category,
+      default_price: price, // use destructuring w/ alias to avoid snake_case
+    },
+  } = props;
 
-);
+  return (
+    <div data-testid="Information">
+      <div>Overview: Product Information</div>
+      {/* Star Rating (# of reviews) */}
+      {/* Product Category */}
+      <p>{category}</p>
+      <p>{name}</p>
+      <p>
+        ${price}
+      </p>
+      {/* Product Overview */}
+      <p>ID {id}</p>
+      {/* Share on Social Media */}
+      <br />
+    </div>
+
+  );
+}
+
+Information.propTypes = {
+  infoList: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    slogan: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    default_price: PropTypes.string.isRequired,
+  })
+};
 
 export default Information;
