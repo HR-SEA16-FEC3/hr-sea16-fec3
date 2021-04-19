f23b10f6ec975c51df5fc0ea1350f5072da9d176
Tony Ly <tonyjly@gmail.com>
2021-04-19 14:16:14 -0700

    add API routes for product information

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
+const products = require('./components/products/products');
 const QandA = require('./components/Q&A/Q&A');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
+app.use(products);
 app.use(QandA);
 
 app.get('/', (req, res) => {
