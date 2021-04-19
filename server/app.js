58a8d73721f469acf1c702a74390816daf12c4a1
zubair-akbar <62559732+zubair-akbar@users.noreply.github.com>
2021-04-19 13:18:27 -0400

    subcomponent framework

--- server/app.js
+++ server/app.js
@@ -1,13 +1,5 @@
 const express = require('express');
 const axios = require('axios');
-<<<<<<< HEAD
-
-const app = express();
-const path = require('path');
-
-// app.use(express.static(path.join(__dirname, '..', 'public')));
-app.use(express.json());
-=======
 const path = require('path');
 const QandA = require('./components/Q&A/Q&A');
 
@@ -16,19 +8,10 @@ const app = express();
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
 app.use(QandA);
->>>>>>> 89a1e8bbec66f0b7c4be6cd05d54252ede8aac0c
 
 app.get('/', (req, res) => {
   res.sendStatus(200);
   console.log('Response from GET console');
 });
 
-<<<<<<< HEAD
-
-  .then((response) => {
-    console.log(response.status);
-  });
-
-=======
->>>>>>> 89a1e8bbec66f0b7c4be6cd05d54252ede8aac0c
 module.exports = app;
