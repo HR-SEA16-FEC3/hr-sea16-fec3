96df39f7559d13425d0d832060f22850b64ff7e2
Tony Ly <tonyjly@gmail.com>
2021-04-20 00:44:16 -0700

    render main image and thumbnails with flexbox

--- client/components/overview/subcomponents/Gallery.jsx
+++ client/components/overview/subcomponents/Gallery.jsx
@@ -8,9 +8,10 @@ const Thumbnail = styled.button`
   background: white;
   padding: 15px;
   &:hover{ background: lightgrey }
+
 `;
 
-const MainImage = styled.image`
+const MainImage = styled.div`
   border: 1px solid black;
   margin-top: 30px;
   margin-right: 30px;
@@ -19,24 +20,30 @@ const MainImage = styled.image`
   &:hover{ background: lightgrey }
 `;
 
+const FlexElement = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  flex-flow: row wrap;
+`;
+
 const Gallery = () => (
 
   <div data-testid="Gallery">
     <div>Overview: Image Gallery</div>
     <br />
-    <p>
+    <FlexElement>
       <MainImage>Main Image</MainImage>
-    </p>
+    </FlexElement>
     <br />
     {/* MAP THUMBNAILS, RENDER INDIVIDUALLY */}
-    <div>
+    <FlexElement>
       <Thumbnail>THUMB 1</Thumbnail>
       <Thumbnail>THUMB 2</Thumbnail>
       <Thumbnail>THUMB 3</Thumbnail>
       <Thumbnail>THUMB 4</Thumbnail>
       <Thumbnail>THUMB 5</Thumbnail>
       <Thumbnail>THUMB 6</Thumbnail>
-    </div>
+    </FlexElement>
 
     <br />
   </div>
