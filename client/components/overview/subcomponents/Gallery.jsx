5961093b3f5ad56b2f30d2da7757ebdaab9e5731
Tony Ly <tonyjly@gmail.com>
2021-04-20 00:23:16 -0700

    render thumbnails and main image placeholders

--- client/components/overview/subcomponents/Gallery.jsx
+++ client/components/overview/subcomponents/Gallery.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Thumbnail = styled.button`
+  border: 1px solid black;
+  margin-top: 15px;
+  margin-right: 15px;
+  background: white;
+  padding: 15px;
+  &:hover{ background: lightgrey }
+`;
+
+const MainImage = styled.image`
+  border: 1px solid black;
+  margin-top: 15px;
+  margin-right: 15px;
+  background: white;
+  padding: 15px;
+  &:hover{ background: lightgrey }
+`;
 
 const Gallery = () => (
 
   <div data-testid="Gallery">
     <div>Overview: Image Gallery</div>
+    <br />
+    <p>
+      <MainImage>Main Image</MainImage>
+    </p>
+
+    {/* MAP THUMBNAILS, RENDER INDIVIDUALLY */}
+    <div>
+      <Thumbnail>THUMB 1</Thumbnail>
+      <Thumbnail>THUMB 2</Thumbnail>
+      <Thumbnail>THUMB 3</Thumbnail>
+      <Thumbnail>THUMB 4</Thumbnail>
+      <Thumbnail>THUMB 5</Thumbnail>
+      <Thumbnail>THUMB 6</Thumbnail>
+    </div>
+
     <br />
   </div>
 
