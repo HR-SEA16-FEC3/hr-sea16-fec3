7b7bd271a9f604b1209bf6dfdcf15f69a481cb5e
Tony Ly <tonyjly@gmail.com>
2021-04-22 21:45:05 -0700

    fetch all products via axios upon loading module

--- client/components/overview/Overview.jsx
+++ client/components/overview/Overview.jsx
@@ -1,4 +1,4 @@
-import React, { useState/* , useEffect */ } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Gallery from './subcomponents/Gallery';
 import Information from './subcomponents/Information';
@@ -7,12 +7,20 @@ import Cart from './subcomponents/Cart';
 import Description from './subcomponents/Description';
 import InfoExample from './product_info_example.json';
 import StylesExample from './product_styles_example.json';
+import axios from 'axios';
 
 function Overview() {
   const [selectedStyle, setSelectedStyle] = useState(0);
+  const [productsList, setProductsList] = useState([]);
   // styles in an array
   // setStyle
   // function(setSelectedStyle), change index in styles array
+
+  useEffect(() => {
+    axios.get('/products')
+      .then((response) => setProductsList(response.data))
+      .catch(() => console.log('Failed to get products'));
+  }, []); // empty dependency array will run effect only once (similar to componentDidMount)
 
   return (
     <div data-testid="Overview">
